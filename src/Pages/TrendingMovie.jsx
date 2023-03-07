c1106b4b69879bc884e1c3794c9bdfd3286ba546
Oleksandr Lytvyniuk <sandro132015@gmail.com>
2023-03-07 16:35:21 +0200

    fix warning

--- src/Pages/TrendingMovie.jsx
+++ src/Pages/TrendingMovie.jsx
@@ -2,12 +2,10 @@ import { getTrendingMovies } from 'api/postAPI';
 import MovieList from 'components/MovieList';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { generatePath, Link, useLocation } from 'react-router-dom';
-import { PATH } from 'router/Path';
 
 const TreningMovie = () => {
   const [trendingMovies, setTrendingMovies] = useState([]);
-  const location = useLocation();
+
   useEffect(() => {
     getTrendingMovies().then(({ data }) => {
       setTrendingMovies(data.results);
